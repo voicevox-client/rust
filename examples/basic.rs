6e78cfd73d024ad1762129c28bb74cc60b296883
tuna2134 <masato.04.11.2007@gmail.com>
2023-04-08 16:34:09 +0900

    bump version

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,14 +1,15 @@
 use voicevox_client::Client;
+use reqwest::Result;
 use std::{io::Write, fs::File};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let client = Client::new("http://localhost:50021".to_string());
     let audio_query = client
         .create_audio_query("こんにちは", 1, None)
-        .await
-        .unwrap();
-    let audio = audio_query.synthesis(1).await.unwrap();
+        .await?;
+    let audio = audio_query.synthesis(1).await?;
     let mut file = File::create("examples/hello.wav").unwrap();
     file.write_all(&audio).unwrap();
+    Ok(())
 }
